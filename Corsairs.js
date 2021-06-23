a2338c82d081051576bf01d8d7d1386afa7fa27b
Nematillo Ochilov <fluctr@mail.ru>
2021-06-23 07:12:16 +0500

    Corsairs o'yinida sun'iy ball to'plash
    Script Nematillo Ochilov tomonidan yozildi.

---
+++ Corsairs.js
@@ -0,0 +1,8 @@
+var mening_balim = 1001;
+var loc = (location.hash || "").substr(1), loc = loc.replace(/[\?&].*/g, "");
+
+function NOHack(dataset, success) {var request = new XMLHttpRequest, tarkib = [], i; for (i in dataset) components.push(encodeURIComponent(i) + "=" + encodeURIComponent(dataset[i]));
+    request.onreadystatechange = function () {4 == request.readyState && 200 == request.status && success(JSON.parse(request.responseText))}; 
+    request.open("POST", "/api/setScore", !0); request.send(tarkib.join("&"))}
+
+NOHack({data: loc, score: mening_balim || 0}, function (a) {console.log("Success"); console.log(a);});
